ae49bb6fa8d5092c604ab1e93ef43fb7f311a273
Mike Matrigali <mikem@apache.org>
2012-03-25 15:06:38 +0000

    DERBY-4275: Query executions fail when compressing a table using SYSCS_UTIL.SYSCS_COMPRESS_TABLE
    backported changes #1142583 and #1160597 from trunk to 10.6 branch.
    
    Move invalidation of dependent statements until the system tables have
    been updated with information about the new conglomerates created by
    compression or truncation. This is to prevent that statements
    executing concurrently get recompiled too early and don't see the new
    conglomerates (and therefore fail on subsequent executions because
    they cannot find the old conglomerates).
    
    Fail in a controlled fashion (StandardException) if the conglomerate
    disappears while binding the FromBaseTable. This used to cause a
    NullPointerException.
    
    Did not backport the changes to TruncateTableTest.  Truncate table 
    functionality was released in 10.7, so changes did not apply to 10.6.
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/db/derby/code/branches/10.6@1305056 13f79535-47bb-0310-9956-ffa450edef68

--- java/engine/org/apache/derby/impl/sql/execute/AlterTableConstantAction.java
+++ java/engine/org/apache/derby/impl/sql/execute/AlterTableConstantAction.java
@@ -392,7 +392,7 @@ class AlterTableConstantAction extends DDLSingleTableConstantAction
 			sd = getAndCheckSchemaDescriptor(dd, schemaId, "ALTER TABLE");
 		}
 		
-		/* Prepare all dependents to invalidate.  (This is there chance
+		/* Prepare all dependents to invalidate.  (This is their chance
 		 * to say that they can't be invalidated.  For example, an open
 		 * cursor referencing a table/view that the user is attempting to
 		 * alter.) If no one objects, then invalidate any dependent objects.
@@ -2088,14 +2088,6 @@ class AlterTableConstantAction extends DDLSingleTableConstantAction
                 TransactionController.MODE_TABLE,
                 TransactionController.ISOLATION_SERIALIZABLE);
 
-		// invalidate any prepared statements that depended on this table 
-        // (including this one), this fixes problem with threads that start up 
-        // and block on our lock, but do not see they have to recompile their 
-        // plan.  We now invalidate earlier however they still might recompile
-        // using the old conglomerate id before we commit our DD changes.
-		//
-		dm.invalidateFor(td, DependencyManager.COMPRESS_TABLE, lcc);
-
 		rl = compressHeapCC.newRowLocationTemplate();
 
 		// Get the properties on the old heap
@@ -2219,6 +2211,10 @@ class AlterTableConstantAction extends DDLSingleTableConstantAction
 		// Update sys.sysconglomerates with new conglomerate #
 		dd.updateConglomerateDescriptor(cd, newHeapConglom, tc);
 
+        // Now that the updated information is available in the system tables,
+        // we should invalidate all statements that use the old conglomerates
+        dm.invalidateFor(td, DependencyManager.COMPRESS_TABLE, lcc);
+
 		// Drop the old conglomerate
 		tc.dropConglomerate(oldHeapConglom);
 		cleanUp();
@@ -2308,15 +2304,6 @@ class AlterTableConstantAction extends DDLSingleTableConstantAction
                                 TransactionController.MODE_TABLE,
                                 TransactionController.ISOLATION_SERIALIZABLE);
 
-		// invalidate any prepared statements that
-		// depended on this table (including this one)
-		// bug 3653 has threads that start up and block on our lock, but do
-		// not see they have to recompile their plan.    We now invalidate earlier
-		// however they still might recompile using the old conglomerate id before we
-		// commit our DD changes.
-		//
-		dm.invalidateFor(td, DependencyManager.TRUNCATE_TABLE, lcc);
-
 		rl = compressHeapCC.newRowLocationTemplate();
 		// Get the properties on the old heap
 		compressHeapCC.getInternalTablePropertySet(properties);
@@ -2402,6 +2389,11 @@ class AlterTableConstantAction extends DDLSingleTableConstantAction
 
 		// Update sys.sysconglomerates with new conglomerate #
 		dd.updateConglomerateDescriptor(cd, newHeapConglom, tc);
+
+        // Now that the updated information is available in the system tables,
+        // we should invalidate all statements that use the old conglomerates
+        dm.invalidateFor(td, DependencyManager.TRUNCATE_TABLE, lcc);
+
 		// Drop the old conglomerate
 		tc.dropConglomerate(oldHeapConglom);
 		cleanUp();
